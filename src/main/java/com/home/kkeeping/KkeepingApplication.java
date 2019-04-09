c172632e99b9652a608bde7deee80c6b7b3ce46f
Gennadii-B <manunification@icloud.com>
2019-04-10 00:56:11 +0400

    Initial commit

---
+++ src/main/java/com/home/kkeeping/KkeepingApplication.java
@@ -0,0 +1,13 @@
+package com.home.kkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkeepingApplication.class, args);
+	}
+
+}
